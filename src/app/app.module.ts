7a933fd3c21bcfba3cbe5bbc7258f411bfd9e6bc
gregorito4 <industria.imagenes.digitales.2020@gmail.com>
2022-04-26 09:54:42 -0500

    Formulario terminado

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
