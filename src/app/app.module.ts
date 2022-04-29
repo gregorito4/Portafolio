60033555afb84dc32e471dc5c6d637e4aa662916
Albeiro Vidal <albeiro.vidal.rojas@gmail.com>
2022-04-29 10:34:51 -0500

    feat: correcion del conflicto merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -22,7 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     
   ],
-=======
+
 
 
   providers: [],
